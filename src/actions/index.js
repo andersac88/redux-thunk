bfce51b44c421b0e0d4682ec808fc76b5dd7fc2a
Andrew Andersen <andersac88@gmail.com>
2020-08-10 21:06:48 -0500

    more practice

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,16 @@ export const fetchPosts = () => async (dispatch) => {
 
 	dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+// THIS WORKS THE SAME WAY
+// export const fetchPosts = () => {
+// 	return async function(dispatch, getState) {
+// 		const response = await jsonplaceholder.get('/posts')
+// 		dispatch({type: 'FETCH_POSTS', payload: response.data})
+// 	}
+// }
+
+export const fetchUser = (id) => async (dispatch) => {
+	const response = await jsonplaceholder.get(`/users/${id}`);
+
+	dispatch({ type: 'FETCH_USER', payload: response.data });
+};
